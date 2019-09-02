a65769fb162be20843d8128c69fbc2c471ee7bc4
Emod Kovacs <emod.kovacs@doclerholding.com>
2019-09-02 16:03:31 +0200

    added description and server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+const { createServer } = require("http");
+const { get } = require("https");
+
+createServer((req, res) => {
+  switch (true) {
+    case /^\/\w{2}\/list-page-ajax\/show-more-json\/0/.test(req.url):
+      get(
+        "https://www.livejasmin.com/en/list-page-ajax/show-more-json/0",
+        response => response.pipe(res)
+      );
+      break;
+    default:
+      get(`https://m.livejasmin.com${req.url}`, response => response.pipe(res));
+  }
+}).listen(3001, "0.0.0.0");
