ccb213a97ca56ce60cd2a2f454be6f3a34514971
makktim <kasatimi01@gmail.com>
2019-09-09 21:55:33 +0200

    basic starter pack and show performers on console

---
+++ gallery/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Performers from "./components/performers/performers";
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+            </header>
+            <Performers/>
+        </div>
+    );
+}
+
+export default App;
