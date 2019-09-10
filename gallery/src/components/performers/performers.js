bad029d7bf4a9f44ba491a101f9bcfec4eb015b9
makktim <kasatimi01@gmail.com>
2019-09-10 13:12:04 +0200

    show performer pictures on the page

--- gallery/src/components/performers/performers.js
+++ gallery/src/components/performers/performers.js
@@ -10,14 +10,24 @@ class Performers extends Component {
 
     componentDidMount() {
         fetch('/en/list-page-ajax/show-more-json/0/')
-            .then(res => res.json())
-            .then(performers => this.setState({performers}, () => console.log('performer list', performers.data.content.performers)));
+            .then(results => results.json())
+            .then(performers => this.setState({'performers': performers.data.content.performers}))
+            // .then(performers => this.setState({performers}, () => console.log('performer list', performers.data.content)));
     }
 
     render(){
         return (
             <div>
                 <h2>performers</h2>
+
+                <div>
+                    {this.state.performers.map(function (item, index) {
+                        return (
+                            {/*<src key={index}>{item.profilePictureUrl}</src>*/},
+                            <img key={index} src={item.profilePictureUrl} alt="text" />
+                        )
+                    })}
+                </div>
             </div>
         );
     }
