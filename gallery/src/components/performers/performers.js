61d7941131816183d6b08358b6de83bbad53e620
makktim <kasatimi01@gmail.com>
2019-09-16 20:39:41 +0200

    showing performer images on their subpage

--- gallery/src/components/performers/performers.js
+++ gallery/src/components/performers/performers.js
@@ -1,27 +1,27 @@
-import React, {Component} from 'react';
-
-class Performers extends Component {
-    constructor() {
-        super();
-        this.state = {
-            performers: []
-        }
-    }
-
-    componentDidMount() {
-        fetch('/en/list-page-ajax/show-more-json/0/')
-            .then(res => res.json())
-            .then(performers => this.setState({performers}, () => console.log('performer list', performers.data.content.performers)));
-    }
-
-    render() {
-        return (
-            <div>
-                <h2>performers</h2>
-            </div>
-        );
-    }
-
-}
-
-export default Performers;
+// import React, {Component} from 'react';
+//
+// class Performers extends Component {
+//     constructor() {
+//         super();
+//         this.state = {
+//             performers: []
+//         }
+//     }
+//
+//     componentDidMount() {
+//         fetch('/en/list-page-ajax/show-more-json/0/')
+//             .then(res => res.json())
+//             .then(performers => this.setState({performers}, () => console.log('performer list', performers.data.content.performers)));
+//     }
+//
+//     render() {
+//         return (
+//             <div>
+//                 <h2>performers</h2>
+//             </div>
+//         );
+//     }
+//
+// }
+//
+// export default Performers;
--- gallery/src/pages/PerformerImages.js
+++ gallery/src/pages/PerformerImages.js
@@ -1,58 +1,143 @@
 import React, {Component} from 'react';
-// import axios from 'axios';
-// import styled from "styled-components";
-// import { Device } from './Device';
+import styled from "styled-components";
+import axios from 'axios';
+import Body from './BodySize';
+import {Device} from './Device';
+import lock from '../images/lock.svg';
 
 
-class PerformerImages extends Component{
+const Gallery = styled.div`
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap-reverse;
+    justify-content: center;
+`;
+
+const ImagePublic = styled.img`   
+    flex: 25%;
+    overflow: hidden;
+    cursor: pointer;
+    background-color: burlywood;
+    padding-top: 5px;
+    width: 100%;
+    height: 100%;
+    &:hover{
+        opacity: 0.5;
+        border: darkred 1px solid;
+        box-shadow: -1px 1px 3px 3px darkred;
+      
+    }
+    
+`;
+
+const ImagePrivate = styled.img`
+    flex: 25%;
+    overflow: hidden;
+    cursor: pointer;
+    background-color: black;
+    padding-top: 5px;
+    width: 100%;
+    height: 100%;
+    &:hover{
+        opacity: 0.5;
+        border: black 1px solid;
+        box-shadow: -1px 1px 3px 3px black;
+
+    }
+
+`;
+
+const GalleryContent = styled.div`
+    
+
+     @media screen and ${Device.mobileS} {
+         flex: 100%;
+         margin: 2px;
+     }
+     
+     @media screen and ${Device.mobileM} {
+         flex: 100%;
+     }
+     
+      @media screen and ${Device.mobileL} {
+         flex: 100%;
+     }
+
+    @media screen and ${Device.tablet} {
+        flex: 49%;
+        margin: 2px;
+    }
+    
+    @media screen and ${Device.laptop}{
+        flex: 33%;
+        margin: 2px;
+    }
+    
+    @media screen and ${Device.laptopL}{
+        flex: 33%;
+        margin: 2px;
+    }
+    
+    @media screen and ${Device.desktop}{
+        flex: 33%;
+        margin: 2px;
+    }
+`;
+
+
+class PerformerImages extends Component {
+
     state = {
-        modelName : null
-    };
+        modelPictures: []
+    }
 
     componentDidMount() {
         let modelName = this.props.match.params.pid;
-        this.setState({modelName: modelName})
+        axios.get('/en/gallery/' + modelName + '/folders' )
+            .then(res => {
+                console.log(res)
+                this.setState({'modelPictures': res.data.data})
+            })
     }
 
-    render(){
-        return(
-            <h2>{this.state.modelName}</h2>
-        )
+    render() {
+        const {modelPictures} = this.state;
+        const modelPictureList = modelPictures.length ? (
+            modelPictures.map(modelPicture => {
+                if(modelPicture.privacy === "exclusive"){
+                    return (
+                        <GalleryContent>
+                                <img className="lockImage" src={lock} alt="" />
+                            <ImagePrivate key={modelPicture.pid} src={modelPicture.previewImageUrl} alt=""/>
+
+                        </GalleryContent>
+                    )
+
+                }else{
+                    return (
+                        <GalleryContent>
+                            <ImagePublic key={modelPicture.pid} src={modelPicture.previewImageUrl} alt=""/>
+                        </GalleryContent>
+                    )
+                }
+
+            })
+        ) : (
+            <GalleryContent>
+                <p>No Models</p>
+            </GalleryContent>
+
+        );
+
+        return (
+            <Body>
+                <Gallery>
+                    {modelPictureList}
+                </Gallery>
+            </Body>
+        );
     }
+
 }
-
-// class PerformerImages extends Component {
-//     state = {
-//         modelName: null
-//     };
-//
-//     componentDidMount() {
-//         let modelName = this.props.match.params.pid;
-//         axios.get('http://localhost:3001/en/list-page-ajax/show-more-json/0/')
-//             .then(res => {
-//                 this.setState({
-//                     modelName: res.data.data.content.performers.pid
-//                 })
-//                 console.log(res)
-//             })
-//         this.setState({
-//             modelName: modelName
-//         })
-//     }
-//
-//     render() {
-//         const modelName = this.state.modelName ? (
-//             <div>
-//                 <h2> {this.state.modelName.pid}</h2>
-//             </div>
-//
-//         ) : (<h2>Loading...</h2>)
-//
-//         return (
-//             <h2> {modelName}</h2>
-//         )
-//     }
-//
-// }
 
 export default PerformerImages;
